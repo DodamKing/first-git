d6807a33c9e242de308097fbc738d019b9581b9b
Dodamking <power6120@naver.com>
2021-08-02 00:57:22 +0900

    예외처리 및 중복회

--- Base ball game(strike ball)/gamecode1.py
+++ Base ball game(strike ball)/gamecode1.py
@@ -4,7 +4,6 @@ answer = str(randint(1, 999))
 
 print(answer)
 
-
 while True:
     guess = input()
 
@@ -16,9 +15,9 @@ while True:
         ball = 0
 
         for i in range(3):
-            if answer[i] == guess[i]:
+            if answer[i] == guess[i]: # 정답의 i번째 값과 추측의 i번째 값이 같으면 스트라이크
                 strike = strike + 1
-            elif guess[i] in answer:
+            elif guess[i] in answer: # 추측한 숫자가 정답에 들어있으면 볼
                 ball = ball +1
 
-        print('{}strike  {}ball'.format(strike, ball))
+        print('{}strike {}ball'.format(strike, ball))
